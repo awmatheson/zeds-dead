311aa0571b29798ab1b74d781883c88d95c940ed
awmatheson <awmatheson@users.noreply.github.com>
2020-03-13 23:55:55 +0000

    deploy: 7f27cd18e743ae9de120f2cf906ad781dfa1e5c6

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://awmatheson.github.io/about/",
     "title": "About Me",
-    "body": "!whistler_pano Born and Raised in Vancouver BC, I am an outdoor enthusiast and interested in the intersection between humanity and technology. I was educated in civil engineering after which I pursued a masters degree in management. As I was always interested in computers and helping people find solutions to problems, which led me to my current pursuits. I currently work @ GitHub in the Data Science org. I focus on building tools for data scientists and machine learning engineers for things from ML Ops to Notebook rendering. If you ever want to reach out with what you think would be an awesome tool GitHub could support or build for the Data Science community, feel free to reach out @MathesonZander on twitter. "
+    "body": " Born and Raised in Vancouver BC, I am an outdoor enthusiast and interested in the intersection between humanity and technology. I was educated in civil engineering after which I pursued a masters degree in management. As I was always interested in computers and helping people find solutions to problems, which led me to my current pursuits. I currently work @ GitHub in the Data Science org. I focus on building tools for data scientists and machine learning engineers for things from ML Ops to Notebook rendering. If you ever want to reach out with what you think would be an awesome tool GitHub could support or build for the Data Science community, feel free to reach out @MathesonZander on twitter. "
     }, {
     "id": 2,
     "url": "https://awmatheson.github.io/categories/",
